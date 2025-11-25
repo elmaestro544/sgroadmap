1894a2a635d9aa03bbc1177a3dd37e05ac5e98d3
elmaestro544 <oxygen.ml1@gmail.com>
2025-11-25 17:51:10 +0300

    Add files via upload

--- env.js
+++ env.js
@@ -20,7 +20,7 @@ window.process.env = {
 
   // --- Supabase ---
   // Get these from your Supabase project settings -> API
-  SUPABASE_URL: 'YOUR_SUPABASE_URL_HERE',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY_HERE',
+  SUPABASE_URL: 'https://zqyifyqplzjmkqhlxcax.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxeWlmeXFwbHpqbWtxaGx4Y2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MTYwNzksImV4cCI6MjA3ODE5MjA3OX0.7OXCAshdOOwu0tE9XAxAef3DHUNYNgAwea_wnB5_vcI',
   
 };
